029c863e2b77efce30b008dab1def38f6d19185c
brilannebays <brilannebays@gmail.com>
2025-03-24 22:21:38 -0500

    Play Screen and Pause UI Panel

---
+++ Assets/Scripts/PauseScreen.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScreen : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+
+    // Update is called once per frame
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            if (gameIsPaused)
+        }
+    }
+}
